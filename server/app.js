7af68341599a626c4dd5031b48f727fd67710603
Amit Nativ <amitnativ11@gmail.com>
2020-12-07 17:04:29 +0200

    basic hello world with express

--- server/app.js
+++ server/app.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+import express from 'express'
+import { connect } from 'mongoose'
+
+connect('mongodb://localhost:27017/mgmtPoc', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const app = express();
+
+app.get('/', (req, res) => res.send('hello world'))
+
+app.listen(9090, () => 'express listening on port 9090')
