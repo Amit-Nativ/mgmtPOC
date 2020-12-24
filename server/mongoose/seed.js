2d0f060827c3a8357a93cbc1cb39ed70de8dcca3
Amit Nativ <amitnativ11@gmail.com>
2020-12-24 11:19:21 +0200

    by id

--- server/mongoose/seed.js
+++ server/mongoose/seed.js
@@ -3,28 +3,27 @@ import roleModel from '../api/role/role.model'
 const roles = [
     {
         id: '1',
-        name: 'root',
-        parent: null
+        name: 'root'
     },
     {
         id: '2',
         name: 'root-child',
-        parent: 'root'
+        parent: '1'
     },
     {
         id: '3',
         name: 'root-second-child',
-        parent: 'root'
+        parent: '1'
     },
     {
         id: '4',
         name: '2-child',
-        parent: 'root-child'
+        parent: '2'
     },
     {
         id: '5',
         name: '4-child',
-        parent: '2-child'
+        parent: '4'
     }
 ]
 
